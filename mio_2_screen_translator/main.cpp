60a073a8b3a668be50f13d280b2e5f1bfb39436e
1MioSubs <it09022022@gmail.com>
2023-04-19 01:10:43 +0300

    Добавьте файлы проекта.

---
+++ mio_2_screen_translator/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+#include <stdlib.h>
+#include <ctime>
+
+using namespace std;
+
+int main() {
+	srand(time(0));
+	string text_in, mes;
+	cout << "Hello World" << endl;
+
+	return 0;
+}
